d78dbbf7fec0b898707e73cb98b3aa3bf924c2eb
L457V01D <tryhard2078@gmail.com>
2020-07-24 00:39:51 +0100

    flag change

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     if (req.body.key === "ilovecheese200311") {
-        res.send("\nFLAG: mycrushrejectedme258")
+        res.send("\nCongrats you have found the flag key! call me xD!\nPHONE NUMBER: 28825413\n")
     } else {
         res.send("\nDON'T BE SO STUPID I'VE ALREADY GAVE YOU THE KEY DUDE!")
     }
 })
 
 app.post('/checkflag', (req, res) => {
-    if (req.body.keyinput === "mycrushrejectedme258") {
+    if (req.body.keyinput === "28825413") {
         res.send("<h3 style='color: green'>The key is correct !</h3>")
     } else {
         res.send("<h3 style='color: red'>The key is not correct !</h3>")
